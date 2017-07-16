491c3b408bfd5610d1f2382fa9bfc7d7165febf6
Hasan ALRUBAYE <halrubia@gmail.com>
2017-07-16 15:05:28 -0600

    creat submite and curated list

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SubmitButtonComponent } from './src/app/component/submit-button/submit-button.component';
+import { CuratedListComponent } from './src/app/component/curated-list/curated-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmitButtonComponent,
+    CuratedListComponent
   ],
   imports: [
     BrowserModule
