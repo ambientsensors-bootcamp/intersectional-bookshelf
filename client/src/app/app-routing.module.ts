c5a14b0fcfe1a66158d91c1f159774c0e92aa593
Sarah King <Buddha@Roxy.local>
2017-07-23 14:43:54 -0600

    Header fits to window, add routing pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './component/about/about.component';
+import { CuratedListComponent } from './component/curated-list/curated-list.component';
+import { SubmitButtonComponent } from './component/submit-button/submit-button.component';
  
  const routes: Routes =
        [
            { path: 'about', component: AboutComponent},
+           { path: 'curated-list', component: CuratedListComponent },
+           { path: 'submit-button', component: SubmitButtonComponent },
            { path: '', pathMatch: 'full', redirectTo: 'about' },
            { path: '**', pathMatch: 'full', redirectTo: 'about' },
 
        ];
-
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { SubmitButtonComponent } from 
-'./component/submit-button/submit-button.component';
-import { CuratedListComponent } from 
-'./component/curated-list/curated-list.component';
+import { SubmitButtonComponent } from './component/submit-button/submit-button.component';
+import { CuratedListComponent } from './component/curated-list/curated-list.component';
 import { HeaderComponent } from './component/header/header.component';
 import { SearchBarComponent } from './component/search-bar/search-bar.component';
 import { MaterialModule } from '@angular/material';
+import { AboutComponent } from './component/about/about.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-
     AboutComponent,
     CuratedListComponent,
     HeaderComponent,
@@ -22,7 +21,7 @@ import { MaterialModule } from '@angular/material';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
