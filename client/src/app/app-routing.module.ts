66d203bef7b72e48c994eb6190720fa776926501
ZahraRahmani <z.zahir2020@gmail.com>
2017-07-29 15:25:14 -0600

    add searchbar router

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './component/about/about.component';
 import { CuratedListComponent } from './component/curated-list/curated-list.component';
 import { SubmitButtonComponent } from './component/submit-button/submit-button.component';
- 
+import { SearchBarComponent } from "./component/search-bar/search-bar.component";
+
+
  const routes: Routes =
        [
            { path: 'about', component: AboutComponent},
            { path: 'curated-list', component: CuratedListComponent },
            { path: 'submit-button', component: SubmitButtonComponent },
+           { path: 'search-bar', component: SearchBarComponent},
            { path: '', pathMatch: 'full', redirectTo: 'about' },
            { path: '**', pathMatch: 'full', redirectTo: 'about' },
+           
 
        ];
 
